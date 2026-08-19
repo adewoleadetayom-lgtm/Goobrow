420e4101d941ecf244d88b5923cf00a71b670cb6
adewoleadetayom-lgtm <adewoleadetayom@gmail.com>
2026-08-20 00:21:49 +0100

    Serve Goobrow web interface from backend

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("www"));
 
 app.get("/api/search", (req, res) => {
   const query = String(req.query.q || "").trim();
