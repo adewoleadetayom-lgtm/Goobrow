4fb6ad5b84f9552a18f61c1015616d4e1a4d4215
adewoleadetayom-lgtm <adewoleadetayom@gmail.com>
2026-08-24 20:08:23 +0100

    Add native Goobrow browser and GitHub APK build

--- android/app/src/main/java/com/goobrow/browser/MainActivity.java
+++ android/app/src/main/java/com/goobrow/browser/MainActivity.java
@@ -1,5 +1,13 @@
 package com.goobrow.browser;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(GoobrowBrowserPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
